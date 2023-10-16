4a30694ced2c4750cbe3a57ae0b32dc26e832a43
ArjunVenkitraman <ramukpully@gmail.com>
2023-10-16 09:56:16 +0530

    Add project files.

---
+++ TrainingLine/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrainingLine.Models;
+using TrainingLine.Repository;
+
+namespace TrainingLine.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+                _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int Id)
+        {
+            return _bookRepository.GetBookById(Id);
+        }
+
+        public List<BookModel> SearchBook(string name, string author) {
+        
+            return _bookRepository.SearchBooks(name, author);
+        }
+    }
+}
---
+++ TrainingLine/Repository/BookRepository.cs
@@ -0,0 +1,33 @@
+﻿using TrainingLine.Models;
+
+namespace TrainingLine.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        } 
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBooks(string Title,string Author)
+        {
+            return DataSource().Where(x => x.Title.Contains(Title) || x.Author.Contains(Author)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>
+            {
+                new BookModel(){Id=1,Title="MVC",Author="Ram"},
+                new BookModel(){Id=1,Title=".Net",Author="Venkit"},
+                new BookModel(){Id=1,Title="Python",Author="Vikram"},
+                new BookModel(){Id=1,Title="Mern",Author="Hari"}
+            };
+        }
+    }
+}
